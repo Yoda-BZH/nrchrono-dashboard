1a238aedb052dcef18862e1fd25e08a8715abc17
Yoda-BZH <yodabzh+gh-f-f2b@gmail.com>
2015-06-19 23:53:33 +0200

    set lang to fr

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -13,12 +13,13 @@ forecast_location_long = "0.1957"
 # "si" - International System of Units
 # "uk" - SI w. windSpeed in mph
 forecast_units = "si"
-  
+forecast_lang = "fr"
+
 SCHEDULER.every '5m', :first_in => 0 do |job|
   http = Net::HTTP.new("api.forecast.io", 443)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-  response = http.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
+  response = http.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}&lang=#{forecast_lang}"))
   forecast = JSON.parse(response.body)  
   forecast_current_temp = forecast["currently"]["temperature"].round
   forecast_current_icon = forecast["currently"]["icon"]
